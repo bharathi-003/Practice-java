04ee19c043295b8793730aeff72a65648bb739c5
bharathi-003 <bharathiananth003@gmail.com>
2026-05-11 11:08:02 +0530

    simple program

---
+++ Practice.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Practice{
+    public static void main(String[] args){
+
+
+        // for(int i=1;i<=10;i++){
+        //     System.out.println(i);
+        // }
+
+
+        // Scanner sc=new Scanner(System.in);
+        // System.out.print("Enter your name:");
+        // String name=sc.next();
+        // System.out.print("Enter your age:");
+        // int age=sc.nextInt();
+        // System.out.print("Enter your hometown:");
+        // String home=sc.next();
+        // System.out.print("Enter you price:");
+        // double price=sc.nextDouble();
+        // System.out.println("My name is:"+name);
+        // System.out.println("My age is:"+age);
+        // System.out.println("My hometown:"+home);
+        // System.out.println("The Price is:"+price);
+
+
+        // String s="mynameis";
+        // int count=0;
+        // for(int i=0;i<=s.length()-1;i++){
+        //     char c=s.charAt(i);
+        //     if(c=='a'|| c=='e'|| c=='i'|| c=='o'||c=='u')
+        //     {
+        //         count++;
+        //     }
+        // }
+
+    }
+}
