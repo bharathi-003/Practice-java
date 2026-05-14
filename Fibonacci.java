ebbc0e19fb61280384f41ce4e5008fa45b61e11c
bharathi-003 <bharathiananth003@gmail.com>
2026-05-14 15:56:12 +0530

    simple programs

---
+++ Fibonacci.java
@@ -0,0 +1,12 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int a=0;
+        int b=1;
+        for(int i=1;i<=5;i++){
+        System.out.print(a+" ");
+        int c=a+b;//0+1
+          a=b;//1
+          b=c;//1
+        }
+        }
+}
