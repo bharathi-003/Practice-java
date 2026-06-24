51b63428d677959a4180ecc781fc04dea8bb60d3
bharathi-003 <bharathiananth003@gmail.com>
2026-06-24 15:57:17 +0530

    Day 2

---
+++ TwoDArray.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class TwoDArray{
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      int m=sc.nextInt();
+      int arr[][]=new int[n][m];
+      int x=sc.nextInt();
+      int y=sc.nextInt();
+      for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            arr[i][j]=sc.nextInt();
+        }
+      }
+       System.out.println(arr[x][y]);
+    }
+}
